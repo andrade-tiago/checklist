c797e79eef165efc0a25de020238b196de19255c
Tiago Andrade <tiagoandradedejoao@gmail.com>
2023-04-06 22:38:24 -0300

    Toda a parte funcional

--- js/main.js
+++ js/main.js
@@ -1,19 +1,89 @@
 const
+	edit      = document.getElementById('edit'),
 	checklist = document.getElementById('checklist'),
-	addItem   = document.getElementById('add');
+	itens     = JSON.parse(localStorage.getItem('itens')) || [];
 
-const addItemToList = () => {
-	const li = document.createElement('li');
-	li.classList.add('checklist-item');
+console.log(edit);
 
-	li.innerHTML = `
-		<div class="checklist-item-check">
-			<input type="checkbox" />
-		</div>
-		<textarea class="checklist-item-text"></textarea>
-	`;
+const generateItem = item => {
+	const checklistItem = document.createElement('li');
+	checklistItem.classList.add('checklist-item');
+	checklistItem.dataset.id = item.id;
 
-	checklist.insertBefore(li, addItem);
+	const check = document.createElement('input');
+	check.classList.add('checklist-item-check');
+	check.type    = 'checkbox';
+	check.checked = item.checked;
+
+	const content = document.createElement('span');
+	content.classList.add('checklist-item-text');
+	content.innerHTML = item.content;
+
+	checklistItem.appendChild(check);
+	checklistItem.appendChild(content);
+	checklistItem.appendChild(createEditButton(item.id));
+	checklistItem.appendChild(createExcludeButton(item.id));
+	checklist.appendChild(checklistItem);
 }
 
-addItem.children[0].addEventListener('click', addItemToList);
+const createEditButton = id => {
+	const button = document.createElement('button');
+
+	button.classList.add('btn', 'bi', 'bi-pencil');
+
+	button.addEventListener('click', () => {
+		const textEdit = edit.elements['text']
+
+		textEdit.value =
+			checklist.querySelector(`[data-id="${id}"] .checklist-item-text`).innerText;
+
+		excludeItem(id);
+		textEdit.focus();
+	})
+
+	return button;
+}
+
+const createExcludeButton = id => {
+	const button = document.createElement('button');
+
+	button.classList.add('btn', 'bi', 'bi-trash');
+
+	button.addEventListener('click', () => {
+		if (confirm('Deletar item?')) {
+			excludeItem(id);
+		}
+	});
+
+	return button;
+}
+
+const excludeItem = id => {
+	itens.splice(itens.findIndex(item => item.id === id), 1);
+	localStorage.setItem('itens', JSON.stringify(itens));
+
+	checklist.querySelector(`[data-id="${id}"]`).remove();
+}
+
+itens.forEach(item => generateItem(item));
+
+edit.addEventListener('submit', event => {
+	event.preventDefault();
+
+	const textItem = edit.elements['text'];
+
+	if (!textItem.value.trim())
+		return alert('Insira um texto.'); 
+
+	const item = {
+		id: itens[itens.length - 1]? (itens[itens.length - 1].id + 1): 0,
+		content: textItem.value,
+		checked: false
+	};
+
+	itens.push(item);
+	localStorage.setItem('itens', JSON.stringify(itens));
+
+	generateItem(item);
+	edit.reset();
+});
